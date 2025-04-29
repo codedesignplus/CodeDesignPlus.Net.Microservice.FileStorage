873de44d07df26c38a7bf15f18d1816008a91d4f
Wilzon Liscano <wliscano@thepalacecompany.com>
2025-04-28 22:41:40 -0500

    Refactor FileStorageControllerTest to consolidate file upload logic and enhance GetFiles test validation

--- tests/integration/CodeDesignPlus.Net.Microservice.FileStorage.Rest.Test/Controllers/FileStorageControllerTest.cs
+++ tests/integration/CodeDesignPlus.Net.Microservice.FileStorage.Rest.Test/Controllers/FileStorageControllerTest.cs
@@ -30,45 +30,51 @@ public class FileStorageControllerTest : ServerBase<Program>, IClassFixture<Serv
     }
 
     [Fact]
+    public async Task GetFiles_ReturnOk()
+    {
+        var id = Guid.NewGuid();
+        var tenant = Guid.NewGuid();
+
+        await this.UploadFileAsync(id, tenant);
+
+        var response = await this.RequestAsync($"http://localhost/api/FileStorage", null, HttpMethod.Get, tenant);
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        var files = System.Text.Json.JsonSerializer.Deserialize<List<FileStorageDto>>(json, this.options)!;
+
+        Assert.NotNull(files);
+        Assert.NotEmpty(files);
+
+        var file = files.FirstOrDefault(x => x.Id == id);
+
+        Assert.NotNull(file);
+        Assert.Equal(id, file.Id);
+        Assert.True(file.Files[0].Success);
+        Assert.Equal("fake.txt", file.Files[0].FileDetail.FullName);
+        Assert.Equal("test", file.Files[0].FileDetail.Metadata.Target);
+
+        Assert.True(Path.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, tenant.ToString(), "test", "fake.txt")));
+    }
+
+
+    [Fact]
     public async Task UploadFile_ReturnOk()
     {
-        var path = AppDomain.CurrentDomain.BaseDirectory;
-        var filePath = Path.Combine(path, "fake.txt");
-
-        var stream = new MemoryStream(System.IO.File.ReadAllBytes(filePath))
-        {
-            Position = 0
-        };
 
         var id = Guid.NewGuid();
         var tenant = Guid.NewGuid();
-        var content = new MultipartFormDataContent
-        {
-            { new StringContent(id.ToString()), "id" },
-            { new StreamContent(stream)
-                {
-                    Headers =
-                    {
-                        ContentType = new MediaTypeHeaderValue("text/plain")
-                    }
-                }, "file", "fake.txt" },
-            { new StringContent("test"), "target" },
-            { new StringContent("false"), "renowned" }
-        };
 
-        var response = await this.RequestAsync("http://localhost/api/FileStorage/upload", content, HttpMethod.Post, tenant);
+        await this.UploadFileAsync(id, tenant);
 
         var file = await this.GetRecordAsync(id, tenant);
-
-        Assert.NotNull(response);
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
         Assert.Equal(id, file.Id);
         Assert.True(file.Files[0].Success);
         Assert.Equal("fake.txt", file.Files[0].FileDetail.FullName);
         Assert.Equal("test", file.Files[0].FileDetail.Metadata.Target);
 
-        Assert.True(Path.Exists(Path.Combine(path, tenant.ToString(), "test", "fake.txt")));
+        Assert.True(Path.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, tenant.ToString(), "test", "fake.txt")));
     }
 
 
@@ -81,33 +87,12 @@ public class FileStorageControllerTest : ServerBase<Program>, IClassFixture<Serv
         var filePath = Path.Combine(path, "fake.txt");
         var fileContentExpected = System.IO.File.ReadAllText(filePath);
 
-        var stream = new MemoryStream(System.IO.File.ReadAllBytes(filePath))
-        {
-            Position = 0
-        };
-
         var id = Guid.NewGuid();
         var tenant = Guid.NewGuid();
-        var content = new MultipartFormDataContent
-        {
-            { new StringContent(id.ToString()), "id" },
-            { new StreamContent(stream)
-                {
-                    Headers =
-                    {
-                        ContentType = new MediaTypeHeaderValue("text/plain")
-                    }
-                }, "file", "fake.txt" },
-            { new StringContent("test"), "target" },
-            { new StringContent("false"), "renowned" }
-        };
 
-        var responseUpload = await this.RequestAsync("http://localhost/api/FileStorage/upload", content, HttpMethod.Post, tenant);
+        await this.UploadFileAsync(id, tenant);
 
-        Assert.NotNull(responseUpload);
-        Assert.Equal(HttpStatusCode.OK, responseUpload.StatusCode);
-
-        var responseDownload = await this.RequestAsync($"http://localhost/api/FileStorage/download/{id}?viewInBrowser={contentDisposition == "inline"  }&file=fake.txt&target=test", null!, HttpMethod.Get, tenant);
+        var responseDownload = await this.RequestAsync($"http://localhost/api/FileStorage/download/{id}?viewInBrowser={contentDisposition == "inline"}&file=fake.txt&target=test", null!, HttpMethod.Get, tenant);
 
         Assert.NotNull(responseDownload);
         Assert.Equal(HttpStatusCode.OK, responseDownload.StatusCode);
@@ -150,6 +135,37 @@ public class FileStorageControllerTest : ServerBase<Program>, IClassFixture<Serv
         }
 
         return response;
+    }
+
+
+    private async Task UploadFileAsync(Guid id, Guid tenant)
+    {
+        var path = AppDomain.CurrentDomain.BaseDirectory;
+        var filePath = Path.Combine(path, "fake.txt");
+
+        var stream = new MemoryStream(System.IO.File.ReadAllBytes(filePath))
+        {
+            Position = 0
+        };
+
+        var content = new MultipartFormDataContent
+        {
+            { new StringContent(id.ToString()), "id" },
+            { new StreamContent(stream)
+                {
+                    Headers =
+                    {
+                        ContentType = new MediaTypeHeaderValue("text/plain")
+                    }
+                }, "file", "fake.txt" },
+            { new StringContent("test"), "target" },
+            { new StringContent("false"), "renowned" }
+        };
+
+        var response = await this.RequestAsync("http://localhost/api/FileStorage/upload", content, HttpMethod.Post, tenant);
+
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 
 
